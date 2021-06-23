2bc21709cb5234dc8f6b4c0ac9a744e16c77a52d
Giulio Rossetti <giulio.rossetti@gmail.com>
2021-06-23 09:11:45 +0200

    :arrow_up: versioning

--- setup.py
+++ setup.py
@@ -3,40 +3,20 @@ from codecs import open
 from os import path
 
 __author__ = 'Giulio Rossetti'
-__license__ = "BSD-2-Clause"
+__license__ = "BSD 2 Clause"
 __email__ = "giulio.rossetti@gmail.com"
 
-
+# Get the long description from the README file
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
 here = path.abspath(path.dirname(__file__))
-
-def get_requirements(remove_links=True):
-    """
-    lists the requirements to install.
-    """
-
-    # try:
-    with open(path.join(here, 'requirements.txt')) as f:
-        requirements = f.read().splitlines()
-    # except Exception as ex:
-    #    with open('DecoraterBotUtils.egg-info\requires.txt') as f:
-    #        requirements = f.read().splitlines()
-    if remove_links:
-        for requirement in requirements:
-            # git repository url.
-            if requirement.startswith("git+"):
-                requirements.remove(requirement)
-            # subversion repository url.
-            if requirement.startswith("svn+"):
-                requirements.remove(requirement)
-            # mercurial repository url.
-            if requirement.startswith("hg+"):
-                requirements.remove(requirement)
-    return requirements
-
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
 
 setup(name='demon',
       version='2.0.6',
@@ -45,6 +25,7 @@ setup(name='demon',
       url='https://github.com/GiulioRossetti/DEMON',
       author='Giulio Rossetti',
       author_email='giulio.rossetti@gmail.com',
+      use_2to3=True,
       classifiers=[
           # How mature is this project? Common values are
           #   3 - Alpha
@@ -68,7 +49,7 @@ setup(name='demon',
           'Programming Language :: Python :: 3'
       ],
       keywords=['complex-networks', 'community discovery'],
-      install_requires=get_requirements(),
+      install_requires=requirements,#['networkx>2.4',  'tqdm', ''],
       long_description=long_description,
       long_description_content_type='text/markdown',
       packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test", "demon.test", "demon.test.*"]),
